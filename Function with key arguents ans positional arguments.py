fbe8c50b691ed85569686dfa6fddd586b13ae40b
Rishav Dasgupta <109132336+FieryScorpion@users.noreply.github.com>
2023-08-29 12:00:31 +0530

    Add files via upload

---
+++ Function with key arguents ans positional arguments.py
@@ -0,0 +1,8 @@
+def greet_with(name, location):
+    print(f"Hello{name}")
+    print(f"What is it like in {location}?")
+
+#greet_with("Jack Bauer", "Nowhere")
+#greet_with("nowhere", "Jack Bauer") #Positional argument
+#functions with Keyword Arguments
+greet_with(location="India", name="Rishav")
